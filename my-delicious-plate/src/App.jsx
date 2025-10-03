6542f796529809bff7b9297deb410d1521d4a7e6
vatsaldhamanwala <dhamanwalavatsal@gamil.com>
2025-10-03 16:29:20 +0530

    Project setup completed.

---
+++ my-delicious-plate/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className=' bg-white flex items-center justify-center'>
+      </div>
+      <h1 className='bg-[rgba(111,78,55,1)] text-3xl text-[#fffcf5] p-4'> Welcome to Delicious Plate </h1>
+    </>
+  )
+}
+
+export default App
