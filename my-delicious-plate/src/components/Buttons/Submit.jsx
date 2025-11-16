3a31b2b0df6bff95dd87438494319231a3fdc24d
vatsaldhamanwala <dhamanwalavatsal@gamil.com>
2025-11-16 19:01:02 +0530

    add cursor pointer property in buttons component.

--- my-delicious-plate/src/components/Buttons/Submit.jsx
+++ my-delicious-plate/src/components/Buttons/Submit.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Submit() {
   return (
     <div className=' rounded-lg w-full max-w-lg mt-12'>
-        <button type='submit' className="w-60 bg-[#6b4226] hover:bg-[#a36234] text-[#fffcf5] font-bold py-3 px-6 rounded-lg mt-3 transition ease-in-out duration-300">
+        <button type='submit' className="cursor-pointer w-60 bg-[#6b4226] hover:bg-[#a36234] text-[#fffcf5] font-bold py-3 px-6 rounded-lg mt-3 transition ease-in-out duration-300">
             Submit
         </button>
     </div >
